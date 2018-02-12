cb8d86b1b080f0d857116596425711eef8a8d8c6
Luis Vieira <luis.m.s.vieira@outlook.pt>
2018-02-12 23:18:25 +0000

    Added nuget.exe and utility .bat

---
+++ pack.bat
@@ -0,0 +1,5 @@
+.\nuget.exe pack .\Robot.Expected\ -Properties Configuration=Release
+.\nuget.exe pack .\Robot.DAL.Core\ -Properties Configuration=Release
+.\nuget.exe pack .\Robot.DAL.Orchestration\ -Properties Configuration=Release
+.\nuget.exe pack .\Robot.DAL.SqlServer\ -Properties Configuration=Release
+pause
