bb6ac68947550adc4e0bb4250fdcc9e72f4602a1
Luis Vieira <luis.m.s.vieira@outlook.pt>
2018-03-08 22:43:01 +0000

    Fixes to packaging.

--- pack.bat
+++ pack.bat
@@ -1,5 +1,5 @@
-.\nuget.exe pack .\Robot.Expected\ -Properties Configuration=Release
-.\nuget.exe pack .\Robot.DAL.Core\ -Properties Configuration=Release
-.\nuget.exe pack .\Robot.DAL.Orchestration\ -Properties Configuration=Release
-.\nuget.exe pack .\Robot.DAL.SqlServer\ -Properties Configuration=Release
+.\nuget.exe pack .\Robot.Expected\ -Properties Configuration=Release -IncludeReferencedProjects
+.\nuget.exe pack .\Robot.DAL.Core\ -Properties Configuration=Release -IncludeReferencedProjects
+.\nuget.exe pack .\Robot.DAL.Orchestration\ -Properties Configuration=Release -IncludeReferencedProjects
+.\nuget.exe pack .\Robot.DAL.SqlServer\ -Properties Configuration=Release -IncludeReferencedProjects
 pause
